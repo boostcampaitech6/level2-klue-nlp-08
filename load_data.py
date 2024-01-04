5deb30c13a619372db120fffbc2d839a8d577afb
ye0ng1 <thdduddn9696@gmail.com>
2024-01-04 09:12:36 +0000

    modularization - first commit

--- load_data.py
+++ load_data.py
@@ -33,7 +33,7 @@ def preprocessing_dataset(dataset):
   out_dataset = pd.DataFrame({'id':dataset['id'], 'sentence':dataset['sentence'],'subject_entity':subject_entity,'object_entity':object_entity,'label':dataset['label'],})
   return out_dataset
 
-# dataset csv파일 경로 (config에 넣어야 할까?)
+# dataset csv파일 경로
 def load_data(dataset_dir):
   """ csv 파일을 경로에 맡게 불러 옵니다. """
   pd_dataset = pd.read_csv(dataset_dir)
