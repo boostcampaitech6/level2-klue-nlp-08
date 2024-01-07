5ef33ab18224083752c452121fe9e70335eef67e
exena <exena01@gmail.com>
2024-01-07 13:52:35 +0900

    Now users don't need to copy link from browser

--- download_from_GC.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-FILE_ID="$1"
-# output_file="filename_after_download"
-# download_dir="/data/ephemeral"
-# gdown "https://drive.google.com/uc?id=${file_id}" -O "${download_dir}/${output_file}"
-gdown "https://drive.google.com/uc?id=${FILE_ID}"
--- upload_data_to_GC.sh
+++ upload_data_to_GC.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-FILE_NAME="$1"
+read -p "파일 이름을 입력하세요: " FILE_NAME
 # 공유 폴더 링크 https://drive.google.com/drive/folders/19fTYxwpJ6rIuSKcVWg1F77PYascRhA_6?usp=drive_link
 UPLOAD_FOLDER="19fTYxwpJ6rIuSKcVWg1F77PYascRhA_6"
 gdrive upload --parent $UPLOAD_FOLDER $FILE_NAME --share
